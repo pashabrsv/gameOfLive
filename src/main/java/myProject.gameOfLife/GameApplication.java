f32886cc8af376483832e088e1f9f46dbcfda481
pashabrsv <pashabrsv@mail.ru>
2022-03-15 21:17:11 +0300

    save changes

---
+++ src/main/java/myProject.gameOfLife/GameApplication.java
@@ -0,0 +1,12 @@
+package myProject.gameOfLife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GameApplication.class, args);
+    }
+}
+
